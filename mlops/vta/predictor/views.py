d86ae931ce9e6a44df1da76303a33df4e53b7d8f
Pablo Schwarzenberg <pabloschwarzenberg@gmail.com>
2023-04-17 15:20:36 -0400

    mlops

--- mlops/vta/predictor/views.py
+++ mlops/vta/predictor/views.py
@@ -11,4 +11,38 @@ class HomePageView(TemplateView):
         return render(request,"index.html",context=None)
     
 def StudentOutcome(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        form = StudentOutcomeForm(request.POST)
+        if form.is_valid():
+            url="http://localhost:8501/v1/models/reprobacion:predict"
+            solved=[]
+            if form.cleaned_data["e0"]:
+                solved.append(1)
+            else:
+                solved.append(0)
+            if form.cleaned_data["e1"]:
+                solved.append(1)
+            else:
+                solved.append(0)
+            if form.cleaned_data["e42"]:
+                solved.append(1)
+            else:
+                solved.append(0)
+            data={"instances":[solved]}
+            header={"Content-Type":"application/json"}
+            result=requests.post(url,data=json.dumps(data),headers=header)
+            if result.status_code == 200:
+                answer=result.json()
+                print(solved)
+                print(answer)
+                if answer["predictions"][0][0] > answer["predictions"][0][1]:
+                    outcome="Pass"
+                else:
+                    outcome="Fail"
+            else:
+                outcome="Unknown"
+            return render(request,"predicted_outcome.html", {"outcome": outcome,"solved":solved})
+    else:
+        form = StudentOutcomeForm()
+
+    return render(request, "student_outcome.html", {"form": form})
